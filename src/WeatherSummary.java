5fc564721c33c435c78d4168af4f83bba764699b
AlexandrBalan <epix246@gmail.com>
2026-04-20 12:57:59 -0700

    defined the variables needed to start, along with implemented the scanner function

--- src/WeatherSummary.java
+++ src/WeatherSummary.java
@@ -1,3 +1,7 @@
+import java.util.Scanner;
+
+
+
 public class WeatherSummary {
     /**
      * Reads newline-delimted temperatures from System.in and prints summary
@@ -16,7 +20,15 @@ public class WeatherSummary {
      * @param args command line arguments (ignored)
      */
     public static void main(String[] args) {
+        //define the variables needed
+        double min = Double.MAX_VALUE;
+        double max = Double.MIN_VALUE;
+        double total = 0;
+        int count = 0;
+
+
         // Implement this method!
         // Hint: use Scanner. nextDouble() and hasNextDouble() will be helpful here!
+        Scanner scan = new Scanner(System.in);
     }
 }
